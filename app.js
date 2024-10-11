3021a3ae4dcfa4291cf7149615aa2946a93544ad
ZahimAhmed18 <159295861+zahimkhan18@users.noreply.github.com>
2024-10-11 19:33:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+
+// question 1
+// var username = prompt("enter your name")
+// var userAge = +prompt("enter your age")
+// alert("Hello! " + username + ",your "+ userAge +" year old")
+
+// question 2
+
+// var num = +prompt("enter number")
+
+// if(num == 0 ){
+//     alert("number is zero")
+// }else if(num < 0 ){
+//     alert("number is negative")
+// }else if(num > 0){
+//     alert("number is positive")
+// }
+
+// questiion 3
+// var arr = [];
+// var val1 = +prompt("value 1")
+// var val2 = +prompt("value 2")
+// var val3 = +prompt("value 3")
+// var val4 = +prompt("value 4")
+// var val5 = +prompt("value 5")
+
+// arr.push(val1)
+// arr.push(val2)
+// arr.push(val3)
+// arr.push(val4)
+// arr.push(val5)
+
+// var total = val1 + val2 + val3 + val4 + val5
+// var sum = total / 5
+// alert(total)
+// alert( "average value is " + sum+"%")
+
+
+// question 4 
+// var arr = [];
+// var city1 = prompt("enter city 1")
+// var city2 = prompt("enter city 2")
+// var city3 = prompt("enter city 3")
+// var city4 = prompt("enter city 4")
+// var city5 = prompt("enter city 5")
+// arr.push(city1)
+// arr.push(city2)
+// arr.push(city3)
+// arr.push(city4)
+// arr.push(city5)
+// for(var i = 0 ; i < arr.length ;i++ ){
+//     alert("city " +(i+1)+" "+ arr[i])
+// }
+
+
+
+
+
+
+
+
+
